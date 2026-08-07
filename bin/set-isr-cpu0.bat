5780b64737eac786147e52ae4827e2d3db28b510
Lucentnoctix <50292962+Lucentnoctix@users.noreply.github.com>
2026-08-08 05:22:01 +0800

    补充中断路由目标负载

--- bin/set-isr-cpu0.bat
+++ bin/set-isr-cpu0.bat
@@ -1,2 +1,4 @@
 powercfg -SetAcValueIndex Scheme_Current Sub_IntSteer Mode 5
 powercfg -SetDcValueIndex Scheme_Current Sub_IntSteer Mode 5
+powercfg -SetAcValueIndex Scheme_Current Sub_IntSteer PerProcLoad 900
+powercfg -SetDcValueIndex Scheme_Current Sub_IntSteer PerProcLoad 900
